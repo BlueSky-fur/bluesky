b6d06b59b1222c336abdb5ef12074104e65afea2
Eleana Deisinger <ellie@cubegaming.eu>
2025-04-05 13:04:37 +0200

    Added Files

---
+++ src/components/Intro.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import "../styles/Intro.css";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import FadeInSection from "./FadeInSection";
+import FractalTree from "./FractalTree";
+
+class Intro extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+      visible: true,
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+  render() {
+    return (
+      <div id="intro">
+        <FractalTree></FractalTree>
+        <Typist avgTypingDelay={120}>
+          <span className="intro-title">
+            {"hi, "}
+            <span className="intro-name">{"Ellie"}</span>
+            {" here. :)"}
+          </span>
+        </Typist>
+        <FadeInSection>
+          <div className="intro-subtitle">I create stuff sometimes.</div>
+          <div className="intro-desc">
+            I'm a tech enthusiast from Hamburg, Germany, with a passion for programming, AI,
+            cloud computing and everything IT with a deeper focus on
+            System Administration and Development Operations(DevOps)
+            I've led a large gaming community and am now building up a
+            new one, bringing people together through technology and innovation.
+          </div>
+          <a
+            href="mailto:ellie.e.deisinger@gmail.com"
+            className="intro-contact"
+          >
+            <EmailRoundedIcon></EmailRoundedIcon>
+            {" Say hi!"}
+          </a>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default Intro;
